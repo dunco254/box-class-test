a289a3103e5046c8b58184fc2d417f2c3354976e
dun254 <109207154+dunreal254@users.noreply.github.com>
2022-08-26 09:28:18 +0000

    box

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 
-
+class box{
+  int height;
+  int length;
+  int width;
+  
+}
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    box obj1= new box();
+    box obj2= new box();
+    obj1.height =1;
+    obj1.width=2 ;
+    obj2.length=1;
+    obj2=obj1;
+    
+    System.out.println(obj2.height);
   }
 }
